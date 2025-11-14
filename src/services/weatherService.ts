2f6cd52a02365b16e1c82bce0188184b27992280
Matheus Coelho <matheus-_-coelho@hotmail.com>
2025-11-14 16:08:07 -0300

    Configuração da API Key via .env.local e ajustes no weatherService

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "85f61eac902c56086e75f16d3ef0b913";
+const API_KEY = import.meta.env.VITE_OPENWEATHER_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeatherByCity = async (city: string) => {
